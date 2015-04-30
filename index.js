04b70f2c40956bb7d492669a554e3c5802c229d8
Sergey Chikuyonok <serge.che@gmail.com>
2015-04-30 19:06:02 +0300

    Working prototype of SocketCluster

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 #!/usr/bin/env iojs --es-staging --harmony_arrow_functions
 'use strict';
 
-var net = require('net');
-var http = require('http');
+var SocketCluster = require('./lib/socket-cluster');
 
-var client = net.connect({port: 9001}, function() {
-	console.log('connected to server');
-})
-client.on('data', function(data) {
-	console.log('received data', data.toString());
-	http.get('http://download.emmet.io/hello.txt', function(res) {
-		res.pipe(client);
-	});
-})
-.on('end', function() {
-	console.log('client closed');
-});
+var cluster = new SocketCluster();
