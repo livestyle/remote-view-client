faf07e00e62da3c3b6c38c51ee185d4a05e616bf
Sergey Chikuyonok <serge.che@gmail.com>
2015-05-16 15:24:18 +0300

    Auto-reconnect when cluster looses connection

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 'use strict';
 
 var TunnelCluster = require('./lib/tunnel-cluster');
+var Tunnel = require('./lib/tunnel');
 
-var cluster = new TunnelCluster({
-	url: 'http://localhost:9001',
-	maxConnections: 6
-});
+module.exports = function(options) {
+	return new TunnelCluster(options);
+};
+module.exports.Tunnel = Tunnel;
+module.exports.TunnelCluster = TunnelCluster;
+
+if (require.main === module) {
+	var cluster = new TunnelCluster({
+		url: 'http://localhost:9001',
+		maxConnections: 6
+	});
+}
