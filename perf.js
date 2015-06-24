495ac5673bf46b183bd48ff96a8fd158b5de9f28
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-25 01:48:21 +0300

    Do not send ack resonse to tunnel
    Added some performance metricks

--- perf.js
+++ perf.js
@@ -6,3 +6,22 @@ var cluster = new TunnelCluster({
 	url: `http://localhost:9001/__test-session`,
 	maxConnections: 10
 });
+
+setInterval(stats, 300).unref();
+
+
+var charm = require('charm')(process);
+charm.reset();
+function stats() {
+	var dy = 1;
+	charm.erase('screen').cursor(false);
+	cluster.tunnels.forEach(function(tunnel, i) {
+		var status = [];
+		tunnel.connected && status.push('connected');
+		tunnel.destroyed && status.push('destroyed');
+		status.push('traffic: ' + tunnel.traffic);
+		
+		charm.position(1, dy++)
+		.write(`Tunnel #${i}: ${status.join(', ')}`);
+	});
+}
