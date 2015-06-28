03503aa4045a01533bdc55dc0659bc65fb7d34b1
Sergey Chikuyonok <serge.che@gmail.com>
2015-06-29 01:32:07 +0300

    Minor tweaks

--- perf.js
+++ perf.js
@@ -7,21 +7,23 @@ var cluster = new TunnelCluster({
 	maxConnections: 10
 });
 
-setInterval(stats, 300).unref();
+setInterval(stats, 100).unref();
 
 
 var charm = require('charm')(process);
 charm.reset();
 function stats() {
 	var dy = 1;
+	var line = function(msg) {
+		charm.position(1, dy++).write(msg);
+	};
 	charm.erase('screen').cursor(false);
 	cluster.tunnels.forEach(function(tunnel, i) {
 		var status = [];
-		tunnel.connected && status.push('connected');
-		tunnel.destroyed && status.push('destroyed');
-		status.push('traffic: ' + tunnel.traffic);
+		tunnel.rvSocket && status.push(tunnel.rvSocket.readyState);
 		
-		charm.position(1, dy++)
-		.write(`Tunnel #${i}: ${status.join(', ')}`);
+		line(`Tunnel #${i}: ${status.join(', ')}`);
 	});
+	line('Total errors: ' + cluster.errors.length);
+	cluster.errors.slice(0, 10).forEach(line);
 }
